39ea41e140aad25b8f80a4047ceda4d95da264a8
oda-gitso <owaind@bath.edu>
2023-01-26 00:39:05 +0700

    Remove comments

--- src/negate.c
+++ src/negate.c
@@ -18,11 +18,8 @@ bi_negate(bi_t to, const bi_t a)
     {
         int abs_size_a = ABS(a->n_digits);
 
-        /* If t->n_alloc < abs_size_a, malloc or realloc depending if 0 or
-         * nonzero number of elements already allocated. */
         to->digits = BI_REALLOC_IF_NECESSARY(to, abs_size_a);
 
-        /* Copy a's digits array to to's digits array */
         BI_DCOPY(to->digits, a->digits, abs_size_a);
 
         to->n_digits = -a->n_digits;
