5a2096320f183e944858ddc04ecd5bf30748eee7
oda-gitso <owaind@bath.edu>
2023-01-25 23:37:26 +0700

    bit_length_digit(): choose function based on preprocessor macros rather than runtime branching.

--- src/bit_length.c
+++ src/bit_length.c
@@ -3,14 +3,12 @@
 ///////////////////////////////////////////////////////////////////////////////
 #include "bi_internal.h"
 
-#include <limits.h> /* ULONG_MAX, CHAR_BIT */
-
 
 ///////////////////////////////////////////////////////////////////////////////
 //  Bit Length
 ///////////////////////////////////////////////////////////////////////////////
 /******************************************************************************
- *  bit_length_digit: Given a `digit`, return the number of bits required to
+ *  bit_length_digit: Given a digit, return the number of bits required to
  *                    represent it.
  ******************************************************************************/
 static uint8_t
@@ -28,23 +26,28 @@ static uint8_t
 _bit_length_u32(uint32_t number)
 {
     uint8_t n_bits = 1;
-    if ((number & 0xffff0000) != 0) {
+    if ((number & 0xffff0000) != 0)
+    {
         number >>= 16;
         n_bits += 16;
     }
-    if ((number & 0xff00) != 0) {
+    if ((number & 0xff00) != 0)
+    {
         number >>= 8;
         n_bits += 8;
     }
-    if ((number & 0xf0) != 0) {
+    if ((number & 0xf0) != 0)
+    {
         number >>= 4;
         n_bits += 4;
     }
-    if ((number & 0xc) != 0) {
+    if ((number & 0xc) != 0)
+    {
         number >>= 2;
         n_bits += 2;
     }
-    if ((number & 0x2) != 0) {
+    if ((number & 0x2) != 0)
+    {
         n_bits += 1;
     }
     return n_bits;
@@ -89,20 +92,13 @@ _bit_length_u64(uint64_t number)
 uint8_t
 bit_length_digit(digit number)
 {
-    static int tot_bits = sizeof(digit) * CHAR_BIT;
-
-    if (tot_bits == 64)
-    {
-        return _bit_length_u64(number);
-    }
-    else if (tot_bits == 32)
-    {
-        return _bit_length_u32(number);
-    }
-    else
-    {
-        return _bit_length_digit(number);
-    }
+  #if BI_DIGIT_BITS == 64
+    return _bit_length_u64(number);
+  #elif BI_DIGIT_BITS == 32
+    return _bit_length_u32(number);
+  #else
+    return _bit_length_digit(number);
+  #endif
 }
 
 
@@ -113,6 +109,9 @@ bit_length_digit(digit number)
  *                 In the event that an unsigned long (ULONG_MAX >= 2**32 - 1)
  *                 is not wide enough to count the number of bits, returns
  *                 ULONG_MAX.
+ *
+ *                 TODO: this check may not be needed. Allocator should detect
+ *                       this in advance.
  ******************************************************************************/
 unsigned long
 bi_bit_length(const bi_t a)
@@ -124,6 +123,6 @@ bi_bit_length(const bi_t a)
         return ULONG_MAX;
     }
 
-    return (abs_ndigits - 1) * BI_SHIFT +                                     \
+    return (abs_ndigits - 1) * BI_DIGIT_BITS +                                \
            bit_length_digit(a->digits[abs_ndigits - 1]);
 }
