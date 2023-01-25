8402d67c6ae27b8c78c39ef9dd6ff34e37acf43a
oda-gitso <owaind@bath.edu>
2023-01-25 23:18:10 +0700

    Remove comments

--- src/set.c
+++ src/set.c
@@ -11,11 +11,8 @@ void bi_set(bi_t to, const bi_t from)
 {
     int from_n_digits = ABS(from->n_digits);
 
-    /* If to->n_alloc < from_n_digits, reallocate and return new digit vector.
-     * Otherwise, return to->digits. */
     to->digits = BI_REALLOC_IF_NECESSARY(to, from_n_digits);
 
-    /* Copy `from_n_digits` elements from from->digits to to->digits. */
     BI_DCOPY(to->digits, from->digits, from_n_digits);
 
     to->n_digits = from->n_digits;
