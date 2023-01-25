8a352712b09142ca9db959b20d3f46a51632b125
oda-gitso <owaind@bath.edu>
2023-01-25 20:49:16 +0700

    Use BI_REALLOC_IF_NECESSARY() and BI_DCOPY() macros instead of function

--- src/abs.c
+++ src/abs.c
@@ -18,12 +18,9 @@ bi_abs(bi_t to, const bi_t a)
     {
         int abs_size_a = ABS(a->n_digits);
 
-        /* If t->n_alloc < abs_size_a, malloc or realloc depending if 0 or
-         * nonzero number of elements already allocated. */
-        to->digits = bi_realloc_if_necessary(to, abs_size_a);
+        to->digits = BI_REALLOC_IF_NECESSARY(to, abs_size_a);
 
-        /* Copy a's digits array to to's digits array */
-        bi_dcopy(to->digits, a->digits, abs_size_a);
+        BI_DCOPY(to->digits, a->digits, abs_size_a);
 
         to->n_digits = abs_size_a;
     }
