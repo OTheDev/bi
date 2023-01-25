6ad108114254342db3f1efd5eaf49cd70edea55c
oda-gitso <owaind@bath.edu>
2023-01-26 01:04:32 +0700

    Remove unnecessary variables in bi_set_bit() and consolidate comments

--- src/bitwise.c
+++ src/bitwise.c
@@ -5,11 +5,17 @@
 
 
 ///////////////////////////////////////////////////////////////////////////////
-//  Includes
+//  Getting/Setting a Bit
+//
+//  Note: when getting/setting a bit, these functions act as if the integer
+//        is nonnegative with the least significant bit at index 0. When
+//        setting a bit, the resulting integer preserves its sign (after its
+//        absolute value has been changed, if at all).
+//
+//        i >= bi_bit_length(a) is permitted.
 ///////////////////////////////////////////////////////////////////////////////
 /******************************************************************************
- *  bi_get_bit: get bit i of a (zero-based indexing), without regard for sign
- *              (i.e. as if a were nonnegative, but preserving its sign).
+ *  bi_get_bit: get/test bit i of a.
  ******************************************************************************/
 bool
 bi_get_bit(bi_t a, unsigned long i)
@@ -27,26 +33,19 @@ bi_get_bit(bi_t a, unsigned long i)
 
 
 /******************************************************************************
- *  bi_set_bit: set bit i of a (zero-based indexing), without regard for sign
- *              (i.e. as if a were nonnegative, but preserving its sign).
- *              i >= bi_bit_length(a) is permitted.
+ *  bi_set_bit: set bit i of a.
  ******************************************************************************/
 void
 bi_set_bit(bi_t a, unsigned long i)
 {
-    int asize, digit_idx, bit_idx;
-    digit mask;
+    int asize, digit_idx;
 
     asize = ABS(a->n_digits);
-
     digit_idx = i / BI_DIGIT_BITS;
-    bit_idx = i % BI_DIGIT_BITS;
-
-    mask = DIGIT_C(1) << bit_idx;
 
     if (digit_idx < asize)
     {
-        a->digits[digit_idx] |= mask;
+        a->digits[digit_idx] |= DIGIT_C(1) << (i % BI_DIGIT_BITS);
     }
     else
     {
@@ -55,6 +54,6 @@ bi_set_bit(bi_t a, unsigned long i)
 
         BI_MEMSET(a->digits + asize, digit_idx - asize, 0);
 
-        a->digits[digit_idx] = mask;
+        a->digits[digit_idx] = DIGIT_C(1) << (i % BI_DIGIT_BITS);
     }
 }
