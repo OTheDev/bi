bc92172f864187b289bc38cf14ca5ee6775eff8b
oda-gitso <owaind@bath.edu>
2023-01-28 01:34:30 +0700

    Address FIXME. Use inequality rather than subtraction that will never be negative.

--- src/rshift.c
+++ src/rshift.c
@@ -8,13 +8,16 @@
 //  Routines
 ///////////////////////////////////////////////////////////////////////////////
 /*****************************************************************************
- *  Shifts digit vector `to_shift[0:size]` `shift_by` bits to the right,
- *  placing the result in `result` (which may be the same as `to_shift`).
- *  Returns the size of result, which may be zero, indicating the value of
- *  the result is zero.
+ *  bi__rshift: Shifts digit vector `to_shift[0:size]` `shift_by` bits to the
+ *              right, placing the result in `result` (which may be the same
+ *              as `to_shift`). Returns the number of digits in `result`,
+ *              which may be zero, indicating the value of the result is
+ *              zero.
+ *
+ *              `size` > 0 assumed.
  *****************************************************************************/
-unsigned
-bi__rshift(digit *result, digit *to_shift, unsigned size, unsigned long shift_by)
+int
+bi__rshift(digit *result, digit *to_shift, int size, bi_bitcount_t shift_by)
 {
     int size_result, hi, shift_bits;
     unsigned long shift_digits;
@@ -23,13 +26,12 @@ bi__rshift(digit *result, digit *to_shift, unsigned size, unsigned long shift_by
     shift_digits = shift_by / BI_SHIFT;
     shift_bits = shift_by % BI_SHIFT;
 
-    size_result = size - shift_digits;
-
-    /* FIXME: this will never be negative. */
-    if (size_result <= 0)
+    if ((unsigned) size <= shift_digits)
     {
         return 0;
     }
+
+    size_result = size - shift_digits;
 
     /* TRUE: size_result >= 1. */
 
@@ -38,7 +40,6 @@ bi__rshift(digit *result, digit *to_shift, unsigned size, unsigned long shift_by
     sum = to_shift[shift_digits];
     sum >>= shift_bits;
 
-    /* FIXME: cmp of integers of different size. */
     for (int i = 0, j = shift_digits + 1; j < size; i++, j++)
     {
         sum += (twodigits)to_shift[j] << hi;
@@ -57,7 +58,7 @@ bi__rshift(digit *result, digit *to_shift, unsigned size, unsigned long shift_by
  *  Otherwise, equivalent to (-1) * (|a| >>= shift_by).
  ******************************************************************************/
 void
-bi_irshift(bi_t a, unsigned long shift_by)
+bi_irshift(bi_t a, bi_bitcount_t shift_by)
 {
     int size = ABS(a->n_digits);
 
