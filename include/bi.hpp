0cb5f834bd3a17a7dfd173b9bea62ad350b29491
OTheDev <116417456+OTheDev@users.noreply.github.com>
2024-02-23 11:03:57 +0700

    Make the separation between impl headers and public headers clear

--- include/bi.hpp
+++ include/bi.hpp
@@ -6,8 +6,6 @@ SPDX-License-Identifier: Apache-2.0
 #ifndef BI_INCLUDE_BI_HPP_
 #define BI_INCLUDE_BI_HPP_
 
-#include <algorithm>
-#include <cassert>
 #include <climits>
 #include <compare>
 #include <iostream>
@@ -15,7 +13,7 @@ SPDX-License-Identifier: Apache-2.0
 #include <string>
 #include <utility>
 
-#include "bi_digit_vector.impl.hpp"
+#include "impl-bi_digit_vector.hpp"
 
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
 static_assert(sizeof(double) * CHAR_BIT == 64, "64-bit double is assumed.");
@@ -174,121 +172,8 @@ BI_API void swap(bi_t& a, bi_t& b) noexcept;
 BI_API bi_t operator"" _bi(const char* str);
 BI_API bi_t abs(const bi_t& value);
 
-/**
- *  @name Accessors for internal representation
- */
-///@{
-
-/// Return the number of digits the allocated storage can hold.
-inline size_t bi_t::capacity() const noexcept { return vec_.capacity(); }
-
-/// Return the number of digits used. Instance represents `0` iff `size() == 0`.
-inline size_t bi_t::size() const noexcept { return vec_.size(); }
-
-/// Reture `true` if the integer is (strictly) negative, false otherwise.
-inline bool bi_t::negative() const noexcept { return negative_; }
-
-/**
- *  @brief Return a read-only span of the digits stored in the internal digit
- *  vector, with least significant digits first. If the integer is zero, an
- *  empty span is returned.
- *
- *  @warning Modifying the integer after the span is returned may invalidate the
- *  span.
- */
-inline std::span<const digit> bi_t::digits() const {
-  if (vec_.size() == 0) {
-    return std::span<const digit>();
-  }
-  return std::span<const digit>(vec_.data(), vec_.size());
-}
-
-///@}
-
-/**
- *  @name Internal modifiers for the digit vector
- */
-///@{
-
-inline void bi_t::reserve_(size_t new_capacity) { vec_.reserve(new_capacity); }
-
-inline void bi_t::resize_(size_t new_size) { vec_.resize(new_size); }
-
-inline digit& bi_t::operator[](size_t index) { return vec_[index]; }
-
-inline const digit& bi_t::operator[](size_t index) const { return vec_[index]; }
-
-inline void bi_t::resize_unsafe_(size_t new_size) {
-  assert(new_size <= vec_.capacity());
-  vec_.resize_unsafe(new_size);
-}
-
-inline void bi_t::trim_trailing_zeros() noexcept {
-  size_t new_size = vec_.size();
-  while (new_size > 0 && vec_[new_size - 1] == 0) {
-    --new_size;
-  }
-  vec_.resize(new_size);
-  if (new_size == 0) {
-    negative_ = false;
-  }
-}
-
-///@}
-
-/**
- *  @name Other
- */
-///@{
-
-/**
- *  @brief Swap the contents of this `bi_t` object with another `bi_t` object.
- *  @complexity O(1)
- */
-inline void bi_t::swap(bi_t& other) noexcept {
-  using std::swap;
-  swap(this->vec_, other.vec_);
-  swap(this->negative_, other.negative_);
-}
-
-/**
- *  @brief Negates the integer in place.
- *  @complexity O(1)
- */
-inline void bi_t::negate() noexcept {
-  if (size() != 0) {
-    negative_ = !negative_;
-  }
-}
-
-/**
- *  @brief Return an `int` indicating the sign of the number: `-1` for negative,
- *  `0` for zero, `1` for positive.
- *  @complexity O(1)
- */
-inline int bi_t::sign() const noexcept {
-  if (size() == 0) {
-    return 0;
-  }
-  return negative_ ? -1 : 1;
-}
-
-/**
- *  @brief Return `true` if this integer is odd, `false` otherwise.
- *  @complexity O(1)
- */
-inline bool bi_t::odd() const noexcept {
-  return size() == 0 ? false : (*this)[0] & 1;
-}
-
-/**
- *  @brief Return `true` if this integer is even, `false` otherwise.
- *  @complexity O(1)
- */
-inline bool bi_t::even() const noexcept { return !odd(); }
-
-///@}
-
 }  // namespace bi
 
 #endif  // BI_INCLUDE_BI_HPP_
+
+#include "impl-bi.inl"
